47b9e26c1b96ad71d58edf3a228865490356152a
Ilya Buzunov <46969838+ZTex275@users.noreply.github.com>
2019-02-20 18:54:05 +0300

    Add files via upload
    переделано в функции

--- test.js
+++ test.js
@@ -1,3 +1,20 @@
-tag= prompt("Введите какой тэг вы хотите найти?" );
-alert("Количество всех тэгов: " + document.getElementsByTagName(tag).length);
+function delay(mymessage, msec) {
+    return function() {
+      var a = this;
+      var b = arguments;
+        setTimeout(function(){mymessage(a,b);}, msec);}
+      }
+  
+
+
+
+
+function mymessage(arg) {
+  console.log('Message: ' + arg);
+}
+
+var superFunc = delay(mymessage, 2000);
+
+superFunc('Привет'); // сообщение отобразится через 2 секунды
+
 
